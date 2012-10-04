d77c2a145a6eee8d7012e19f3c1bfde75fb8c312
Traxmaxx <github@netzok.net>
2012-10-04 14:25:18 +0200

    Added create method to credit card

--- lib/paymill/credit_card.rb
+++ lib/paymill/credit_card.rb
@@ -1,5 +1,7 @@
 module Paymill
   class CreditCard
+    include Paymill::Operations::Create
+
     attr_accessor :id, :card_type, :country, :expire_month, :expire_year,
                   :card_holder, :last4, :created_at, :updated_at
 
