2a163801b80116984f5e72d98fe1df1725ebc5cd
Traxmaxx <github@netzok.net>
2012-10-05 16:14:01 +0200

    Updated subscription test for the collection support

--- spec/paymill/subscription_spec.rb
+++ spec/paymill/subscription_spec.rb
@@ -32,9 +32,13 @@ describe Paymill::Subscription do
   end
 
   describe ".find" do
-    it "makes a new GET request using the correct API endpoint" do
+    it "makes a new GET request using the correct API endpoint to receive a specific subscription" do
       Paymill.should_receive(:request).with(:get, "subscriptions/123", {}).and_return("data" => {})
       Paymill::Subscription.find("123")
+    end
+    it "makes a new GET request using the correct API endpoint to receive all subscriptions" do
+      Paymill.should_receive(:request).with(:get, "subscriptions/", {}).and_return("data" => {})
+      Paymill::Subscription.find("")
     end
   end
 
