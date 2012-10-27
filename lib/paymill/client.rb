747ecc81eb8da800fc6a7f467e82737b7cbbfb66
Max Power <kevin.melchert@gmail.com>
2012-10-27 23:24:33 +0300

    remove attr_accessor for attributes in client

--- lib/paymill/client.rb
+++ lib/paymill/client.rb
@@ -1,7 +1,7 @@
 module Paymill
   class Client < Model
     extend Paymill::Operations::Delete
-    attr_accessor :id, :email, :description, :attributes, 
+    attr_accessor :id, :email, :description,
                   :created_at, :updated_at,
                   :creditcard, :subscription
   end
