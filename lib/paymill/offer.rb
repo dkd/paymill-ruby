b0d23618f5841f1818a6f76a9df0d0b505e996bd
Max Power <kevin.melchert@gmail.com>
2012-10-28 16:18:50 +0100

    Update lib/paymill/offer.rb

--- lib/paymill/offer.rb
+++ lib/paymill/offer.rb
@@ -1,6 +1,6 @@
 module Paymill
   class Offer < Model
     extend Paymill::Operations::Delete
-    attr_accessor :id, :name, :amount, :interval, :trial_period_days, :currency
+    attr_accessor :id, :name, :amount, :interval, :trial_period_days, :currency, :created_at, :updated_at
   end
 end
