e50e6272dc692a357b236d0e55da9dcdcb4e6f88
Max Power <kevin.melchert@gmail.com>
2012-10-28 16:18:28 +0100

    Update lib/paymill/subscription.rb

--- lib/paymill/subscription.rb
+++ lib/paymill/subscription.rb
@@ -2,7 +2,8 @@ module Paymill
   class Subscription < Model
     extend Paymill::Operations::Delete
     attr_accessor :id, :plan, :livemode, :cancel_at_period_end,
-                  :canceled_at, :client, :created_at, :updated_at
+                  :canceled_at, :client, :created_at, :updated_at,
+                  :offer, :trial_start, :trial_end, :customer
 
   end
 end
