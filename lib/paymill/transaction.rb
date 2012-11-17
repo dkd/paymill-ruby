547ac58112a94a01135892d9815f180ee14f4dfc
Kevin <kevin.melchert@gmail.com>
2012-11-17 16:56:40 +0100

    format the documentation

--- lib/paymill/transaction.rb
+++ lib/paymill/transaction.rb
@@ -4,18 +4,22 @@ module Paymill
     # important to know is that only transactions under a amount of 10,000.00 € will be processed.
     
     # https://www.paymill.com/de-de/dokumentation/referenz/api-referenz/index.html#document-transactions
-    # id, string, Unique identifier of this transaction
-    # amount, string, Formatted amount of this transaction
-    # origin_amount, integer (>0), The used amount, smallest possible unit per currency (for euro, we’re calculating the amount in cents)
-    # status, enum(open, pending, closed, failed, partial_refunded, refunded, preauthorize), Indicates the current status of this transaction, e.g closed means the transaction is sucessfully transfered, refunded means that the amount is fully or in parts refunded.
-    # description, string or null, Need a additional description for this transaction? Maybe your shopping cart ID or something like that?
-    # livemode, boolean,  Whether this transaction was issued while being in live mode or not
-    # payment, hash, creditcard-object or directdebit-object
-    # client, hash or null, clients-object
-    # preauthorization, hash or null, preauthorizations-object
-    # created_at, integer, Unix-Timestamp for the creation date
-    # updated_at, integer, Unix-Timestamp for the last update
-    # 
+    # id:            String, Unique identifier of this transaction
+    # amount:        String, Formatted amount of this transaction
+    # origin_amount: Integer (>0), The used amount, smallest possible unit per currency 
+    #                (for euro, we’re calculating the amount in cents)
+    # status:        Enum(open, pending, closed, failed, partial_refunded, refunded, preauthorize)
+    #                Indicates the current status of this transaction, e.g closed means the transaction is sucessfully transfered, refunded means that the amount is fully or in parts refunded.
+    # description:   String or null, 
+    #                Need a additional description for this transaction? 
+    #                Maybe your shopping cart ID or something like that?
+    # livemode:      Boolean,  Whether this transaction was issued while being in live mode or not
+    # payment:       Hash, creditcard-object or directdebit-object
+    # client:        Hash or null, clients-object
+    # preauthorization: Hash or null, preauthorizations-object
+    # created_at:    Integer, Unix-Timestamp for the creation date
+    # updated_at:    Integer, Unix-Timestamp for the last update
+
     # Create new Transaction with ...
     # - Token
     # - Payment
