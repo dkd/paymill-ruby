64c5b93bda9ea4a3862597f454589a8a26d9f5c0
Kevin <kevin.melchert@gmail.com>
2012-11-17 15:53:27 +0100

    new persisted? method

--- lib/paymill/concerns/persistence.rb
+++ lib/paymill/concerns/persistence.rb
@@ -4,6 +4,10 @@ module Paymill
       def new?
         id.nil?
       end
+      
+      def persisted?
+        !new?
+      end
     
       def save
         new? ? create : update
@@ -15,27 +19,14 @@ module Paymill
       alias :destroy :delete
     
       def reload
-#        load self.class.find(id).attributes unless new?
       end
     
       private
     
       def update
-  #      data = attributes_for_update
-  #      load self.class.update(id, data).attributes
       end
     
       def create
-  #      data = attributes_for_create
-  #      load self.class.create(data).attributes
-      end
-      
-      def attributes_for_create
-        attributes.splice(*self.class.valid_attributes_for_create)
-      end
-      
-      def attributes_for_update
-        attributes.splice(*self.class.valid_attributes_for_update)
       end
     end
   end
