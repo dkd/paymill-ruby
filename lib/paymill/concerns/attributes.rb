132aeda75df730a5677edc14561029a44a264473
Kevin <kevin.melchert@gmail.com>
2012-11-17 16:12:08 +0100

    attributes

--- lib/paymill/concerns/attributes.rb
+++ lib/paymill/concerns/attributes.rb
@@ -8,24 +8,13 @@ module Paymill
     
       def read_attribute(name)
         instance_variable_get("@#{name}".to_sym)
-#        @attributes.fetch(name, nil)
       end
       alias :[] :read_attribute
     
       def write_attribute(name, value)
         instance_variable_set("@#{name}".to_sym, value)
-#        @attributes[name] = value
       end
       alias :[]= :write_attribute
-
-
-      def created_at
-        read_time_attribute(:created_at)
-      end
-    
-      def updated_at
-        read_time_attribute(:updated_at)
-      end
 
       private
     
@@ -37,19 +26,6 @@ module Paymill
             write_attribute(attr, value)
           end
         end
-      end
-      
-      def read_money_attribute(name)
-        Money.new(read_attribute(:name))
-      end
-      
-      def read_time_attribute(name)
-        value = read_attribute(name)
-        Time.at(value) unless value.nil?
-      end
-      
-      def read_array_attribute(name, klass)
-        (read_attribute(name) || []).map { |d| klass.new(d) }
       end
       
     end
