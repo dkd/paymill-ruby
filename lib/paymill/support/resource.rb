538b1b2e90b28af5c1d2e9cc41f7b48c37bdff36
Kevin <kevin.melchert@gmail.com>
2012-11-17 15:58:49 +0100

    move resource

--- lib/paymill/support/resource.rb
+++ lib/paymill/support/resource.rb
@@ -3,16 +3,17 @@ module Paymill
     extend Concerns::Naming
     extend Concerns::Crud
     extend SingleForwardable
-    # #def_delegators :scoped, :where, :order, :limit, :offset, :each, :all, :first, :last, :count
+
     delegate [:where, :order, :limit, :offset, :paginate, :each, :all, :first, :last, :count, :delete_all, :empty?, :total] => :scoped
-    
+
     def self.scoped
       Paymill::Scope.new(self)
     end
-    
+
     include Concerns::Attributes
     include Concerns::Persistence
-    
-    attr_reader :id
+
+    attr_accessor :id, :created_at, :updated_at
   end
 end
+
