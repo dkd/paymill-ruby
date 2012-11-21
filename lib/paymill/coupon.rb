fe47662fc5a60b71a822c84613b5ad4955b25b4b
Kevin <kevin.melchert@gmail.com>
2012-11-21 19:38:54 +0100

    seperate fixed value and percent value coupons

--- lib/paymill/coupon.rb
+++ lib/paymill/coupon.rb
@@ -3,5 +3,32 @@ module Paymill
     attr_accessor :name, :type, :fixed_value, :percent_value
     attr_accessor :usage_time, :valid_from, :valid_until, :max_usable
     attr_accessor :min_transaction_volume, :max_transaction_volume, :transaction_type
+    
+    def self.build(attrs={})
+      case attrs['type'] || attrs[:type]
+        when 'fixed_value'   then Coupon::Fixed.new(attrs)
+        when 'percent_value' then Coupon::Percent.new(attrs)
+      end
+    end
+    
+    class Fixed < Coupon
+      def self.scoped
+        super.where(type: 'fixed_value')
+      end
+    
+      def value
+        fixed_value
+      end
+    end
+  
+    class Percent < Coupon
+      def self.scoped
+        super.where(type: 'percent_value')
+      end
+    
+      def value
+        percent_value
+      end
+    end
   end
 end
