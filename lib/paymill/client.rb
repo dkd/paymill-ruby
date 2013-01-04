2972625ae2d7cf5d90f606e81be47b87c755d884
Christoph Sassenberg <christoph.sassenberg@googlemail.com>
2013-01-04 12:58:50 +0000

    adding poor man's update to clients

--- lib/paymill/client.rb
+++ lib/paymill/client.rb
@@ -2,8 +2,9 @@ module Paymill
   class Client < Base
     include Paymill::Operations::All
     include Paymill::Operations::Create
-    include Paymill::Operations::Delete
     include Paymill::Operations::Find
+    include Paymill::Operations::Update
+    include Paymill::Operations::Delete
 
     attr_accessor :id, :email, :description, :attributes, :created_at,
                   :updated_at, :payment, :subscription
