68af577b203292e31c94b1761d96f7aa3cb2cd59
André Cedik <andre@webionate.de>
2013-01-24 12:19:04 +0100

    added token for querying cc data

--- lib/paymill/payment.rb
+++ lib/paymill/payment.rb
@@ -1,6 +1,6 @@
 module Paymill
   class Payment < Base
     attr_accessor :id, :card_type, :country, :expire_month, :expire_year,
-                  :card_holder, :last4, :created_at, :updated_at
+                  :card_holder, :last4, :created_at, :updated_at, :token
   end
 end
