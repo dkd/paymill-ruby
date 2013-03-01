e0bdef0c2d0ec7a906e086652274ea3aeec62fe2
abarre <a@fasterize.com>
2013-03-01 13:55:32 +0100

    Fix #19 : add Paymill::Payment.delete method.

--- lib/paymill/payment.rb
+++ lib/paymill/payment.rb
@@ -1,5 +1,7 @@
 module Paymill
   class Payment < Base
+    include Paymill::Operations::Delete
+
     attr_accessor :id, :card_type, :country, :expire_month, :expire_year,
                   :card_holder, :last4, :created_at, :updated_at
   end
