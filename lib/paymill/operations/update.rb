7d1ba6b8d1eb4b52c1390571d42061c67e9a1880
Nerian <siotopo@gmail.com>
2013-05-17 19:33:38 +0200

    Add the #update_attributes method to Update. This takes an id and a Hash of attributes to update.

--- lib/paymill/operations/update.rb
+++ lib/paymill/operations/update.rb
@@ -1,6 +1,21 @@
 module Paymill
   module Operations
     module Update
+
+      module ClassMethods
+        # Updates a object
+        # @param [Integer] id The id of the object that should be updated
+        # @param [Hash] attributes The attributes that should be updated
+        def update_attributes(id, attributes)
+          response = Paymill.request(:put, "#{self.name.split("::").last.downcase}s/#{id}", attributes)
+          self.new(response["data"])
+        end
+      end
+
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+
       # Updates a object
       #
       # @param [Hash] attributes The attributes that should be updated
