45230966212b6fbdae6fe3d74909fe9a3bdaa0c0
Zamith <zamith.28@gmail.com>
2013-06-02 15:50:23 +0100

    Removes some data dependencies

--- lib/paymill/request/base.rb
+++ lib/paymill/request/base.rb
@@ -4,22 +4,33 @@ require_relative 'validator'
 module Paymill
   module Request
     class Base
+      attr_reader :info
       def initialize(info)
         @info = info
-        @connection = Connection.new(@info)
-        @validator = Validator.new(@info)
       end
 
       def send
         raise AuthenticationError if Paymill.api_key.nil?
-        @connection.setup_https
+        connection.setup_https
         send_request
-        return @validator.validated_data_for(@response)
+        return validator.validated_data_for(response)
       end
 
       private
       def send_request
-        @response = @connection.request
+        @response = connection.request
+      end
+
+      def response
+        @response
+      end
+
+      def connection
+        @connection ||= Connection.new(info)
+      end
+
+      def validator
+        @validator ||= Validator.new(info)
       end
     end
   end
