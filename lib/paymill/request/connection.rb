5059f7b9baaa81dd2376140c3fdd3a2e6e392445
Zamith <zamith.28@gmail.com>
2013-07-27 10:03:25 +0100

    Removing duplicate code

--- lib/paymill/request/connection.rb
+++ lib/paymill/request/connection.rb
@@ -1,36 +1,3 @@
-class Hash
-  def flatten_keys(newhash={}, keys=nil)
-    self.each do |k, v|
-      k = k.to_s
-      keys2 = keys ? keys+"[#{k}]" : k
-      if v.is_a?(Hash)
-        v.flatten_keys(newhash, keys2)
-      else
-        newhash[keys2] = v
-      end
-    end
-    newhash
-  end
-end
-
-def normalize_params(params, key=nil)
-  params = params.flatten_keys if params.is_a?(Hash)
-  result = {}
-  params.each do |k,v|
-    case v
-      when Hash
-        result[k.to_s] = normalize_params(v)
-      when Array
-        v.each_with_index do |val,i|
-          result["#{k.to_s}[#{i}]"] = val.to_s
-        end
-      else
-        result[k.to_s] = v.to_s
-    end
-  end
-  result
-end
-
 module Paymill
   module Request
     class Connection
